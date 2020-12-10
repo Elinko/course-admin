08f68bc6f906aa90200dea9c578c651100bcc394
Patrik Eliáš <>
2020-12-10 08:18:47 +0100

    Initial CI and start design hp and and admin hp

---
+++ app/Controllers/AdminHP.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+class AdminHP extends BaseController
+{
+	public function index()
+	{
+
+		return view('admin-hp');
+	}
+
+	//--------------------------------------------------------------------
+
+}
---
+++ app/Views/partials/layouts/main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<!--
+	Forty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Roland Izsof</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/main.css" />
+		<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/custom.css" />
+		<noscript><link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+ 
+
+		<!-- Scripts -->
+			<script src="<?php echo base_url(); ?>/assets/js/jquery.min.js"></script>
+			<script src="<?php echo base_url(); ?>/assets/js/jquery.scrolly.min.js"></script>
+			<script src="<?php echo base_url(); ?>/assets/js/jquery.scrollex.min.js"></script>
+			<script src="<?php echo base_url(); ?>/assets/js/browser.min.js"></script>
+			<script src="<?php echo base_url(); ?>/assets/js/breakpoints.min.js"></script>
+			<script src="<?php echo base_url(); ?>/assets/js/util.js"></script>
+			<script src="<?php echo base_url(); ?>/assets/js/main.js"></script>
+
+	</body>
+</html>
